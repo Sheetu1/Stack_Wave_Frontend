f5582573d1022c58d3c1f0b6833042d2c8c7baae
Sheetu1 <sheetusharma548@gmail.com>
2025-04-28 16:47:24 +0530

    Update BASE_URL to correct backend URL for production environment

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,2 @@
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://stack-wave-backend.onrender.com"
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://stack-wave-backend-5c4t.onrender.com"
